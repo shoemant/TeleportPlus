3ff3854e9df22920c23e42881bdca06aa5bd0430
shoemant <sumantdhir2@gmail.com>
2024-09-27 02:08:56 -0700

    Added .jar to repository

---
+++ src/test/java/com/Sumant/TeleportPlus/HomeCommandTest.java
@@ -0,0 +1,19 @@
+package com.Sumant.TeleportPlus.HomeCommandTest;
+
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class HomeCommandTest {
+}
